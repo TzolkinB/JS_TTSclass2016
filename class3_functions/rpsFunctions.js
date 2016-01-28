6afd70a4908333768a36a69dd7f5ca389b062e12
TzolkinB <krbell4@gmail.com>
2016-01-27 22:16:37 -0500

    Now to get everything in the correct order

--- class3_functions/rpsFunctions.js
+++ class3_functions/rpsFunctions.js
@@ -20,17 +20,25 @@ function playRound(player1, player2) {
   	else if ((player1.hand === 'rock' && player2.hand === 'scissors') || 
   		(player1.hand === 'scissors' && player2.hand === 'paper') ||
   		(player1.hand === 'paper' && player2.hand === 'rock')) {
-  		console.log((player1Score += 1) + (player2Score += 0));
-		console.log(player1.name + " wins")
+  		player1Score += 1;
+		console.log(player1.name + " wins this round")
 	}
 	else {
 		console.log(player2Score += 1)
-		console.log(player2.name + " wins")
+		console.log(player2.name + " wins this round")
 	}
 	return (player1.name + ': ' + player1.hand + " vs " + player2.name + ': ' + player2.hand);
 
   };
 
-for i = 0
+while (player1Score < 3 && player2Score < 3) {
+    if (player1Score == 3)
+    	console.log("Katy wins the game!!")
+  //    console.log(player1.name + " wins the game!!"); 
+//player1.name not defined bc it is in playRound function
+    else
+       console.log("Kim wins the game!!");
 console.log(playRound());
+}
+
 
